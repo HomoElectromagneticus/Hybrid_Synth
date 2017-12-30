d6c7da89aef70d8923b0626d79cb1ad2f7c397db
Homo Electromagneticus <homoelectromagneticus@protonmail.com>
2017-12-30 12:46:16 -0800

    Converted all tabs to spaces for sanity's sake.

--- FPGA_verilog/Wavetable_RAM.v
+++ FPGA_verilog/Wavetable_RAM.v
@@ -6,31 +6,31 @@
 //=============================================================================
 
 module Wavetable_RAM(
-	i_clock, 
-	i_address, 
-	i_data_in, 
-	i_write_enable, 
-	o_data_out
+    i_clock, 
+    i_address, 
+    i_data_in, 
+    i_write_enable, 
+    o_data_out
 );
 
-	// inputs and outputs
-	input 			i_clock;
-	input [15:0]	i_address;
-	input [7:0]		i_data_in;
-	input			i_write_enable;
-	
-	output [7:0]	o_data_out;
-	
-	// registers and wires
-	reg [7:0] 		memory [0:65535];	// 64kB of memory using 8 bit words
-	reg [7:0]		o_data_out;
-	
-	// function
-	always @(posedge i_clock) begin
-		if (i_write_enable) begin
-			memory[i_address] <= i_data_in;
-		end
-		o_data_out <= memory[i_address];
-	end
-	
+    // inputs and outputs
+    input 			i_clock;
+    input [15:0]	i_address;
+    input [7:0]		i_data_in;
+    input			i_write_enable;
+    
+    output [7:0]	o_data_out;
+    
+    // registers and wires
+    reg [7:0] 		memory [0:65535];	// 64kB of memory using 8 bit words
+    reg [7:0]		o_data_out;
+    
+    // function
+    always @(posedge i_clock) begin
+        if (i_write_enable) begin
+            memory[i_address] <= i_data_in;
+        end
+        o_data_out <= memory[i_address];
+    end
+    
 endmodule
